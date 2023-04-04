22ee2eab2b0e26ec2b0dd03d764c60811693ba05
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-04 18:19:34 +0530

    Added file for routes

---
+++ src/router/routes.ts
@@ -0,0 +1,12 @@
+const routes = {
+    public: {
+        signin: '/signin',
+        signup: '/signup',
+        forgotPassword: '/forgot-password',
+    },
+    private: {
+        dashboard: '/dashboard',
+    },
+};
+
+export default routes;
