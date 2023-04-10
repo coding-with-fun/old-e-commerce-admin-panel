773ffc638a95eb13a1df82a3cd402da481d272ea
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-10 17:07:19 +0530

    [REVERT] - Reverted image upload

--- src/pages/private/Profile/index.tsx
+++ src/pages/private/Profile/index.tsx
@@ -1,42 +1,10 @@
 import Box from '@mui/material/Box';
-import type React from 'react';
-import { useState } from 'react';
-import { useUploadForm } from '../../../hooks/uploadFile';
-import { baseURL } from '../../../libs/interceptor';
 import DetailsForm from './DetailsForm';
 // import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
-
-interface PostData {
-    image: File | null;
-}
 
 const Profile = (): JSX.Element => {
     // const userDetails = useAppSelector((state) => state.user.userDetails);
     // const dispatch = useAppDispatch();
-
-    const [formValues, setFormValues] = useState<PostData>({
-        image: null,
-    });
-
-    const { isSuccess, uploadForm, progress, size } = useUploadForm(
-        `${baseURL}/get-files-url`
-    );
-
-    const handleImageChange = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ): void => {
-        setFormValues((prevFormValues) => ({
-            ...prevFormValues,
-            image: event.target.files != null ? event.target.files[0] : null,
-        }));
-    };
-
-    const handleSubmit = async (): Promise<void> => {
-        const formData = new FormData();
-        formValues.image != null &&
-            formData.append('filesToUpload', formValues.image);
-        await uploadForm(formData);
-    };
 
     return (
         <Box
@@ -48,28 +16,6 @@ const Profile = (): JSX.Element => {
                 width: '100%',
             }}
         >
-            <div>
-                {isSuccess ? 'true' : 'false'}
-                {progress}
-                <input
-                    multiple
-                    type="file"
-                    name=""
-                    id=""
-                    onChange={handleImageChange}
-                />
-                <button
-                    onClick={() => {
-                        void (async () => {
-                            await handleSubmit();
-                        })();
-                    }}
-                >
-                    Submit
-                </button>
-                {size}MB
-            </div>
-
             <DetailsForm />
         </Box>
     );
